4778dfbc0587f06abf441096aa3cb282942c3bd9
IAmBatby <iambatby@gmail.com>
2024-09-07 12:29:47 +1000

    Initial Heatmap Esque Visual

--- ExtendedPathfinding/ExtendedPathfinding/NodeGrid.cs
+++ ExtendedPathfinding/ExtendedPathfinding/NodeGrid.cs
@@ -4,9 +4,11 @@ using System.Linq;
 using System.Text;
 using UnityEngine;
 using UnityEngine.AI;
+using UnityEngine.UIElements;
 
 namespace ExtendedPathfinding.ExtendedPathfinding
 {
+    [ExecuteAlways]
     public class NodeGrid : MonoBehaviour
     {
         public Vector3 spawnRange;
@@ -20,71 +22,82 @@ namespace ExtendedPathfinding.ExtendedPathfinding
 
         public static int WalkableArea;
 
-        public float navMeshSampleDistance;
 
         [Range(0, 100000)]
         public float maxCubes;
 
-        public Vector3 gridScale;
-
-        bool drawInvisible;
+        public Vector3[,,] nodeGridMatrix;
 
         public Color Invisible = new Color(0, 0, 0, 0);
+
+        public Vector3 gridScale;
+        public float navMeshSampleDistance;
+        public bool drawInvisible;
+        public bool rebakeButtonBool;
+        public bool bakeAlways;
+        public bool useMatrixData;
 
         private void Start()
         {
 
         }
 
-        private void OnGUI()
+
+        private void Update()
         {
-            GUILayout.BeginArea(new Rect(Screen.width - 100, Screen.height - 100, 100, 50), new GUIStyle());
-            if (GUILayout.Button("Rebake Grid"))
+            if (rebakeButtonBool == true || bakeAlways == true)
+            {
                 BakeGridData();
+                rebakeButtonBool = false;
+            }
         }
 
         private void BakeGridData()
         {
-
+            PopulateNodeGridMatrix();
         }
 
         private void OnDrawGizmos()
         {
             if (meshFilter == null) return;
             if (enabled == false) return;
+            if (nodeGridMatrix == null) return;
             WalkableArea = 1 << NavMesh.GetAreaFromName("Walkable");
 
-            //startingPosition = (meshFilter.transform.position - meshFilter.transform.lossyScale) + (gridScale / 2) - meshFilter.sharedMesh.bounds.min;
-
-            //DrawGridCube(startingPosition);
-
-            //startingPosition.x += gridScale.x;
-
-            //DrawGridCube(startingPosition);
             Gizmos.matrix = transform.localToWorldMatrix;
-            foreach (Vector3 position in GetUnits())
-            {
-                Vector3 nodePosition = GetStartingPosition() + position;
-                Color nodeColor = ValidatePosition(nodePosition);
-                DrawGridCube(nodePosition, nodeColor);
-            }
-                
+            GetUnits();
 
-            //foreach (Vector3 position in GetUnits())
-                //Gizmos.DrawWireCube(meshFilter.transform.position - (meshFilter.transform.lossyScale / 2) + position, gridScale);
         }
 
         private Vector3 GetStartingPosition()
         {
             Vector3 value = (transform.position - transform.lossyScale / 2) + (gridScale / 2);
-            //value -= new Vector3(0.5f, 0.5f, 0.5f);
             value -= transform.lossyScale / 2;
             return (value);
         }
 
-        public List<Vector3> GetUnits()
+        public void GetUnits()
         {
-            List<Vector3> returnList = new List<Vector3>();
+            int drawCount = 0;
+            for (int y = 0; y < spawnRange.y; y++)
+            {
+                for (int z = 0; z < spawnRange.z; z++)
+                {
+                    for (int x = 0; x < spawnRange.x; x++)
+                    {
+                        Vector3 nodePosition = GetStartingPosition() + nodeGridMatrix[x,y,z];
+                        Color nodeColor = ValidatePosition(nodePosition);
+                        DrawGridCube(nodePosition, nodeColor);
+                        drawCount++;
+                        if (drawCount > maxCubes) return;
+                    }
+                }
+            }
+        }
+
+        private void PopulateNodeGridMatrix()
+        {
+            nodeGridMatrix = new Vector3[(int)spawnRange.x, (int)spawnRange.y, (int)spawnRange.z];
 
             for (int y = 0; y < spawnRange.y; y++)
             {
@@ -92,13 +105,11 @@ namespace ExtendedPathfinding.ExtendedPathfinding
                 {
                     for (int x = 0; x < spawnRange.x; x++)
                     {
-                        returnList.Add(new Vector3(gridScale.x * x,gridScale.y * y,gridScale.z * z));
-                        if (returnList.Count > maxCubes) return (returnList);
+                        Vector3 adjustedPosition = new Vector3(gridScale.x * x, gridScale.y * y, gridScale.z * z);
+                        nodeGridMatrix[x, y, z] = adjustedPosition;
                     }
                 }
             }
-
-            return (returnList);
         }
 
         private void DrawGridCube(Vector3 position, Color color)
